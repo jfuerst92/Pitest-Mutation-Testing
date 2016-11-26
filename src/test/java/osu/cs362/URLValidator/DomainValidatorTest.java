94d30ac2aef2eb9c2bff12cf86c51a809472deed
Joseph Fuerst <fuerstj@oregonstate.edu>
2016-11-26 15:10:51 -0800

    Create DomainValidatorTest.java

---
+++ src/test/java/osu/cs362/URLValidator/DomainValidatorTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
+import static org.mockito.Mockito.*;
+
+public class DomainValidatorTest {
+
+  @Test
+  public void Test1() {
+    DomainValidator dv = mock(DomainValidator.class);
+    DomainValidator domv = dv.getInstance(true);
+    String localDomain = "127.0.0.1";
+    assertTrue(domv.isValid(localDomain) == true);
+  }
+
+  @Test
+  public void Test2() {
+    DomainValidator dv = mock(DomainValidator.class);
+    DomainValidator domv = dv.getInstance(false);
+    String localDomain = "127.0.0.1";
+    assertTrue(domv.isValid(localDomain) == false);
+  }
+
+  
+}
